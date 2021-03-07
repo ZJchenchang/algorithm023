07b9f66221a1fa1cd2c3a4f5f2695e6e02c93a08
ZJchenchang <zjchenchang@outlook.com>
2021-03-07 23:23:09 +0800

    第九周作业

---
+++ Week_09/[541]反转字符串 II.py
@@ -0,0 +1,35 @@
+# 给定一个字符串 s 和一个整数 k，你需要对从字符串开头算起的每隔 2k 个字符的前 k 个字符进行反转。 
+# 
+#  
+#  如果剩余字符少于 k 个，则将剩余字符全部反转。 
+#  如果剩余字符小于 2k 但大于或等于 k 个，则反转前 k 个字符，其余字符保持原样。 
+#  
+# 
+#  
+# 
+#  示例: 
+# 
+#  输入: s = "abcdefg", k = 2
+# 输出: "bacdfeg"
+#  
+# 
+#  
+# 
+#  提示： 
+# 
+#  
+#  该字符串只包含小写英文字母。 
+#  给定字符串的长度和 k 在 [1, 10000] 范围内。 
+#  
+#  Related Topics 字符串 
+#  👍 117 👎 0
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def reverseStr(self, s: str, k: int) -> str:
+        a = list(s)
+        for i in xrange(0, len(a), 2 * k):
+            a[i:i + k] = reversed(a[i:i + k])
+        return "".join(a)
+# leetcode submit region end(Prohibit modification and deletion)
